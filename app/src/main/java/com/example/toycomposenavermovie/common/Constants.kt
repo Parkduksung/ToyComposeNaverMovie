4c68a3fe1ed7ee2700021d0569ee9ae04d20765d
Parkduksung <duksung1234@naver.com>
2022-03-21 14:29:04 +0900

    [feature] Naver 영화 api 추가.

---
+++ app/src/main/java/com/example/toycomposenavermovie/common/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.toycomposenavermovie.common
+
+object Constants {
+    const val BASE_NAVER_URL = "https://openapi.naver.com/"
+    const val CLIENT_ID = "qRkvFxV_lat8Ge_LbatQ"
+    const val SECRET_KEY = "cVIQ3iWHU7"
+}
---
+++ app/src/main/java/com/example/toycomposenavermovie/data/remote/NaverApi.kt
@@ -0,0 +1,28 @@
+package com.example.toycomposenavermovie.data.remote
+
+import com.example.toycomposenavermovie.common.Constants.CLIENT_ID
+import com.example.toycomposenavermovie.common.Constants.SECRET_KEY
+import com.example.toycomposenavermovie.data.remote.response.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+import java.util.*
+
+interface NaverApi {
+
+    @GET("v1/search/movie.json")
+    suspend fun getMovieList(
+        @Header("X-Naver-Client-Id") clientId: String = CLIENT_ID,
+        @Header("X-Naver-Client-Secret") secretKey: String = SECRET_KEY,
+        @Query("query") keyWord: String,
+        @Query("display") display: Int = INIT_DISPLAY,
+        @Query("yearfrom") searchRangeYearStart: Int = START_YEAR,
+        @Query("yearto") searchRangeYearEnd: Int = END_YEAR
+    ): MovieResponse
+
+    companion object {
+        private const val INIT_DISPLAY = 10
+        private const val START_YEAR = 2000
+        private var END_YEAR = Calendar.getInstance().get(Calendar.YEAR)
+    }
+}
