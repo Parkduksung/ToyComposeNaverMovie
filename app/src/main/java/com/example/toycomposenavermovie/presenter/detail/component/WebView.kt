738e695c6dbeefd1108d0d882a65379155f81931
Parkduksung <duksung1234@naver.com>
2022-03-22 00:52:56 +0900

    [feature] WebView 화면 구성.

---
+++ app/src/main/java/com/example/toycomposenavermovie/presenter/detail/component/WebView.kt
@@ -0,0 +1,42 @@
+package com.example.toycomposenavermovie.presenter.detail.component
+
+import android.graphics.Bitmap
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WebView(url: String) {
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
+    AndroidView(
+        modifier = Modifier.fillMaxSize(),
+        factory = { context ->
+            WebView(context).apply {
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                webViewClient = object : WebViewClient() {
+                    override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                        backEnabled = view.canGoBack()
+                    }
+                }
+                settings.javaScriptEnabled = true
+                loadUrl(url)
+                webView = this
+            }
+        }, update = {
+            webView = it
+        })
+
+    BackHandler(enabled = backEnabled) {
+        webView?.goBack()
+    }
+}
